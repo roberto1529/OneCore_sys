3a116a9aa7f3d1233b8416d3b8bd89c9754f36b2
Roberto Molina Gonzalez <45692969+roberto1529@users.noreply.github.com>
2025-01-07 11:57:37 -0500

    Vacio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { PrimeNG } from 'primeng/config';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'OneCore_sys';
+export class AppComponent implements OnInit  {
+  title = 'OneCore Sys';
+  constructor(private primeng: PrimeNG) {}
+
+  ngOnInit() {
+      this.primeng.ripple.set(true);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideAnimationsAsync(),
+        provideRouter(routes),
+        providePrimeNG({
+            theme: {
+                preset: Aura,
+                options: {
+                  prefix: 'p',
+                  darkModeSelector: 'light',
+                  cssLayer: false
+              }
+            }
+        }),
+        
+    ]
 };
