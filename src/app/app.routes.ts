2b23f4eff6740260a8f3fe2a11e3fedaec5d58ff
Roberto Molina <45692969+roberto1529@users.noreply.github.com>
2025-01-12 20:42:34 -0500

    12-01-2024
    Componente login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AuthComponent },  // Ruta predeterminada  
+    { path: 'login', component: AuthComponent },  // Ruta para About
+    { path: '**', component: AuthComponent }  
+];
