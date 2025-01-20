49a0a7ad41916663766df72516b2d6bb42ff976f
Roberto Molina <45692969+roberto1529@users.noreply.github.com>
2025-01-19 19:59:43 -0500

    19-01-2025
    Animaciones.
    
    Separació0n de companestes en el login

--- src/app/shared/auth/auth.component.ts
+++ src/app/shared/auth/auth.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { IconFieldModule } from 'primeng/iconfield';
@@ -7,43 +6,30 @@ import { InputIconModule } from 'primeng/inputicon';
 import { DatePickerModule } from 'primeng/datepicker';
 import { InputOtpModule } from 'primeng/inputotp';
 import { ThemeService } from '../../services/theme.service';
-import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [ReactiveFormsModule, ButtonModule, FloatLabelModule,IconFieldModule,InputIconModule,
-    DatePickerModule, InputOtpModule, CommonModule],
+  imports: [ButtonModule, FloatLabelModule,IconFieldModule,InputIconModule,
+    DatePickerModule, InputOtpModule, CommonModule, FormularioComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
 export class AuthComponent implements OnInit {
-  fb: FormGroup;
   icons:string= "pi pi-moon";
   severity: 'success' | 'info' | 'warn' | 'danger' | 'help' | 'primary' | 'secondary' | 'contrast' | null | undefined = 'secondary';
   theme: string='';
 
-  constructor(private formBuilder:FormBuilder,
-              public themeService: ThemeService,
-              private _router: Router
-            ){
-    this.fb = this.formBuilder.group({
-      usuario: ['', [Validators.required, Validators.minLength(3)]],
-      pass: ['', [Validators.required, Validators.minLength(6)]],
-      // email: ['', [Validators.required, Validators.email]],
-      token: ['']
-    });
+  constructor(public themeService: ThemeService){
+
   }
 
 
 
   ngOnInit(): void {}
 
-  public mensajeError(campo: string, error: string): boolean {
-    const control = this.fb.get(`${campo}`);
-    return control! && control.hasError(error) && (control.dirty || control.touched);
-  }
 
   public ColorTheme():void {
 
@@ -62,28 +48,5 @@ export class AuthComponent implements OnInit {
 
   }
 
-
-  public onSubmit(): void {
-    let error: boolean = false;
-
-
-      for (const control in this.fb.controls) {
-        const controls = this.fb.get(control) as FormControl
-        if (controls && controls.invalid) {
-          controls.markAsTouched()
-          error = true
-        }
-      }
-
-      if (error) {
-        return
-      }
-
-      if (this.fb.value.usuario ==='rmol' && this.fb.value.pass ==='123456') {
-         this._router.navigate(['/modulos']);
-         console.log(this.fb.value);
-      }
-
-  }
 
 }
