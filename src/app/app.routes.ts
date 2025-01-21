7ee128ab0cfdb3f96a553cf63caae23cbf7eb60c
Roberto Molina <45692969+roberto1529@users.noreply.github.com>
2025-01-21 09:44:17 -0500

    Ing rob (#6)
    * 07-01-25
    
    Menu y Auth
    
    * Revert "07-01-25"
    
    This reverts commit 1525d1c0919c7bab8915cb4f8e71b07c3b3ff886.
    
    * 12-01-2024
    
    Componente login
    
    * mejoras en el temas
    
    * mejoras en el temas
    
    * mejoras en el temas
    
    * mejoras en el temas
    
    * 13-01-2025
    
    Final
    
    * modulos de seleccion de modulo contratado
    
    * Seleccionar contrto
    
    * Seleccionar contrto
    
    * 18-01-2025
    
    Anamaciones login
    
    * 19-01-2025
    
    Animaciones.
    
    Separació0n de companestes en el login
    
    * Primeras implementacion Auth, Encriptado, implementado de json para evacion de varibles de entorno
    
    * 20-01-2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,10 @@ import { NotfoundComponent } from './shared/notfound/notfound.component';
 import { ModsComponent } from './shared/mods/mods.component';
 
 export const routes: Routes = [
-    { path: '', component: AuthComponent, pathMatch: 'full' }, // Ruta predeterminada
-    { path: 'login', component: AuthComponent },
+    { path: '', component: AuthComponent, pathMatch: 'full', title : 'OneCore ERP' }, // Ruta predeterminada
+    { path: 'login', component: AuthComponent, title : 'OneCore ERP' },
     { path: 'modulos', component: ModsComponent, title: 'Modulos Contratados' },
-    { path: '**', component: NotfoundComponent }              // Ruta 404
+    { path: '**', component: NotfoundComponent, title: 'Acceso no autorizado' }              // Ruta 404
+
   ];
 
